f52c7e4e49cfbb20b9eec1189109ef4442cdb2f2
Jeffrey Shimbo <jts@local>
2021-12-01 00:31:11 -0800

    Clean up day 1, part 2

--- 2021/01-b.py
+++ 2021/01-b.py
@@ -1,26 +1,18 @@
 def main():
     count = 0
-    index = 0
     prev_depth = False
     window = []
     with open("01-input") as fp:
         for line in fp:
-            if index < 3:
-                window.append(int(line))
-                index += 1
-                if index == 3:
-                    prev_depth = window[0] + window[1] + window[2]
-                continue
-            else:
-                window[0] = window[1]
-                window[1] = window[2]
-                window[2] = int(line)
-
-            depth = window[0] + window[1] + window[2]
-
-            if depth > prev_depth:
-                count += 1
-            prev_depth = depth
+            window.append(int(line))
+            if len(window) > 3:
+                window.pop(0)
+                depth = window[0] + window[1] + window[2]
+                if depth > prev_depth:
+                    count += 1
+                prev_depth = depth
+            elif len(window) == 3:
+                prev_depth = window[0] + window[1] + window[2]
 
     print(count)
 
