8f11fe94306944bff9b06b7c59d6f631a3910c01
Jeffrey Shimbo <jts@local>
2021-12-04 12:13:07 -0800

    Fix annoyance in Day 3, part 1

--- 2021/03-a.py
+++ 2021/03-a.py
@@ -27,33 +27,30 @@ def main():
 
     gamma = []
     first_line = True
-    num_lines = 0
 
     with open("03-input") as fp:
         for line in fp:
             line = line.strip()
-            num_bits = len(line)
+
             if first_line:
-                first_line = False
-                for x in range(num_bits):
-                    if line[x] == "1":
-                        gamma.append(1)
-                    else:
-                        gamma.append(-1)
-            else:
-                for x in range(num_bits):
-                    if line[x] == "1":
-                        gamma[x] += 1
-                    else:
-                        gamma[x] -= 1
+                # allocate empty vector
+                first_line = not first_line
+                num_bits_range = range(len(line))
+                for x in num_bits_range:
+                    gamma.append(0)
+
+            for x in num_bits_range:
+                if line[x] == "1":
+                    gamma[x] += 1
+                else:
+                    gamma[x] -= 1
 
     print(gamma)
     gamma_decimal = gamma2dec(gamma)
     print(gamma_decimal)
     epsilon_decimal = epsilon2dec(gamma)
     print(epsilon_decimal)
-    print("answer")
-    print(epsilon_decimal * gamma_decimal)
+    print("Answer = ", epsilon_decimal * gamma_decimal)
 
 
 if __name__ == '__main__':
