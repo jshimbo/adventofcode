39f45d1219dfd350076e1576747ffd9713f03fea
Jeffrey Shimbo <jts@local>
2021-12-07 08:00:43 -0800

    Rewrite 2021 day 1 with list windows; unify parts

--- 2021/01-b.py
+++ 2021/01-b.py
@@ -1,20 +1,31 @@
-def main():
-    count = 0
-    prev_depth = False
-    window = []
-    with open("01-input") as fp:
-        for line in fp:
-            window.append(int(line))
-            if len(window) > 3:
-                window.pop(0)
-                depth = window[0] + window[1] + window[2]
-                if depth > prev_depth:
-                    count += 1
-                prev_depth = depth
-            elif len(window) == 3:
-                prev_depth = window[0] + window[1] + window[2]
+"""
+Rewrote original solution to use Python list windows
+  instead of indicies
+"""
 
-    print(count)
+
+def solve(lines, window_size):
+    prev_sum = 0
+    increased = 0
+
+    for i in range(len(lines) - window_size + 1):
+        window = lines[i: i + window_size]
+        new_sum = sum(window)
+
+        if prev_sum and (new_sum > prev_sum):
+            increased += 1
+
+        prev_sum = new_sum
+
+    return increased
+
+
+def main():
+    input_file = "01-input"
+    lines = list(map(int, open(input_file).readlines()))
+
+    print("Answer to part 1:", solve(lines, 1))
+    print("Answer to part 2:", solve(lines, 3))
 
 
 if __name__ == '__main__':
